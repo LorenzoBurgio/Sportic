ab1adcc48746234fe5eca899829169b8d1bc795a
Lorenzo burgio <lorenzo.burgio@epita.fr>
2022-02-24 17:31:32 +0000

    database implementation

--- Sportic/app/src/main/java/com/android/sportic/LoginActivity.java
+++ Sportic/app/src/main/java/com/android/sportic/LoginActivity.java
@@ -83,7 +83,7 @@ public class LoginActivity extends AppCompatActivity {
 
 
 
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+
             }
         });
     }
--- Sportic/app/src/main/java/com/android/sportic/MainActivity.java
+++ Sportic/app/src/main/java/com/android/sportic/MainActivity.java
@@ -1,5 +1,6 @@
 package com.android.sportic;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,15 +11,41 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    FirebaseAuth fauth;
+    FirebaseFirestore fstore;
+    String userID;
+    TextView print;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fauth = FirebaseAuth.getInstance();
+        fstore = FirebaseFirestore.getInstance();
+        TextView print = (TextView) findViewById(R.id.Print);
+
+        userID = fauth.getCurrentUser().getUid();
+
+        DocumentReference documentReference = fstore.collection("users").document(userID);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                print.setText("Welcome " + value.getString("name"));
+            }
+        });
+
     }
 
     public void logout (View view){
