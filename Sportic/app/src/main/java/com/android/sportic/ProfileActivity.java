c56d01a8c7f66fdaef157a0d3abab34ba44800c5
InsMtb <78259423+InsMtb@users.noreply.github.com>
2022-03-22 15:14:11 +0000

    Profile interface + Update Profile Picture

--- Sportic/app/src/main/java/com/android/sportic/ProfileActivity.java
+++ Sportic/app/src/main/java/com/android/sportic/ProfileActivity.java
@@ -1,4 +1,57 @@
 package com.android.sportic;
 
-public class ProfileActivity {
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class ProfileActivity extends AppCompatActivity{
+
+    ImageView imageView;
+    android.widget.Button OpenCamera;
+
+    @Override
+    protected void onCreate(android.os.Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        imageView = findViewById(R.id.image_view);
+        OpenCamera = findViewById(R.id.open_camera);
+
+        if(ContextCompat.checkSelfPermission(ProfileActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(ProfileActivity.this,
+                    new String[]
+                            {
+                                    Manifest.permission.CAMERA
+                            },100);
+        }
+        OpenCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                android.content.Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,100);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode,data);
+
+        if(requestCode == 100)
+        {
+            android.graphics.Bitmap captureImage = (android.graphics.Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(captureImage);
+        }
+    }
 }
