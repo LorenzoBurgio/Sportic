edfaaf0e50aa06ad0f90d5469213a74c9fd6e7ff
InsMtb <78259423+InsMtb@users.noreply.github.com>
2022-03-26 18:28:52 +0000

    Everything without the database

--- Sportic/app/src/main/java/com/android/sportic/ProfileActivity.java
+++ Sportic/app/src/main/java/com/android/sportic/ProfileActivity.java
@@ -5,26 +5,61 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class ProfileActivity extends AppCompatActivity{
 
+    FirebaseAuth fauth;
     ImageView imageView;
+    String userID;
     android.widget.Button OpenCamera;
+    TextView FirstName;
+    TextView LastName;
+    TextView FullName;
+    TextView Location;
+    Button ValidateProfile;
+    EditText firstname;
+    EditText lastname;
+    EditText location;
+    FirebaseFirestore fstore;
 
     @Override
     protected void onCreate(android.os.Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
+        setContentView(R.layout.activity_edit_profile);
+        setContentView(R.layout.activity_main);
+        fauth = FirebaseAuth.getInstance();
         imageView = findViewById(R.id.image_view);
         OpenCamera = findViewById(R.id.open_camera);
+        FirstName = findViewById(R.id.firstName);
+        FullName = findViewById(R.id.fullName);
+        LastName = findViewById(R.id.lastName);
+        Location = findViewById(R.id.location);
+        ValidateProfile = findViewById(R.id.validateProfile);
+        firstname = findViewById(R.id.editfirstName);
+        lastname = findViewById(R.id.editlastName);
+        location = findViewById(R.id.editlocation);
+
+        fauth = FirebaseAuth.getInstance();
+        fstore = FirebaseFirestore.getInstance();
+        userID = fauth.getCurrentUser().getUid();
+        DocumentReference documentReference = fstore.collection("users").document(userID);
+        FullName.setText(FirstName.getText() + " " + LastName.getText());
 
         if(ContextCompat.checkSelfPermission(ProfileActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
 
@@ -39,6 +74,15 @@ public class ProfileActivity extends AppCompatActivity{
             public void onClick(View view) {
                 android.content.Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(intent,100);
+            }
+        });
+        ValidateProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirstName.setText((CharSequence) firstname);
+                LastName.setText((CharSequence) lastname);
+                Location.setText((CharSequence) location);
+                FullName.setText(FirstName.getText() + " " + LastName.getText());
             }
         });
     }
